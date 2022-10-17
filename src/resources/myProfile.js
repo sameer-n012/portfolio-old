81156470b00886cdd16a5a7cfcab4c842f2c6a41
sameer-n012 <39223223+sameer-n012@users.noreply.github.com>
2022-10-17 00:55:31 -0500

    updated profile

--- src/resources/myProfile.js
+++ src/resources/myProfile.js
@@ -1,11 +1,11 @@
 const profileDetails = {
-	email: 'snarendran@wisc.edu',
+	email: 'sameer.narendran@gmail.com',
 	resumeLink:
 		'https://drive.google.com/file/d/1DAvo7yurIF6XlGv5TtwE01F_2SnpUQnF/view',
 	githubLink: 'https://github.com/sameer-n012',
 	linkedinLink: 'https://www.linkedin.com/in/sameer-narendran-84825318a',
-	aboutMeDescription: `I'm Sameer Narendran, a first-year student at the University of Wisconsin Madison
-                        studying Computer Science and Math. My primary interest is in building neural networks, 
+	aboutMeDescription: `I'm Sameer Narendran, a second-year student at the University of Wisconsin Madison
+                        studying Computer Science and Math. My primary interest is in AI and neural networks, 
                         but I also enjoy working on software projects in Java as well as full-stack 
                         applications. When I'm not in class or on my computer, I can usually 
                         be found reading science fiction novels or playing tennis outside with my friends.`,
